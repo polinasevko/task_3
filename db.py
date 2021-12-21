c1b10239ede6f07d99e1a0364df0ee2245b84edd
Polina Sevko <polinasevko24@gmail.com>
2021-12-21 12:15:34 +0300

    Add possibility to choose DBMS

--- db.py
+++ db.py
@@ -5,19 +5,34 @@ LOG = logging.getLogger('db.py')
 
 
 class Database:
-    def __init__(self, **kwargs):
+    dbms_connector = {
+        'mysql': mysql.connector
+    }
+
+    def __init__(self, dbms_name, **kwargs):
+        connector = Database._parse_dbms_name(dbms_name)
+
         try:
-            self._db = mysql.connector.connect(**kwargs)
-        except mysql.connector.Error as err:
-            if err.errno == mysql.connector.errorcode.ER_ACCESS_DENIED_ERROR:
+            self._db = connector.connect(**kwargs)
+        except connector.Error as err:
+            if err.errno == connector.errorcode.ER_ACCESS_DENIED_ERROR:
                 LOG.exception(f"Wrong user name {kwargs.setdefault('user')} or password {kwargs.setdefault('password')}")
-            elif err.errno == mysql.connector.errorcode.ER_BAD_DB_ERROR:
+            elif err.errno == connector.errorcode.ER_BAD_DB_ERROR:
                 LOG.exception(f"Database {kwargs.setdefault('database')} does not exist")
             else:
                 LOG.exception(err)
             exit(1)
 
         self._cursor = self._db.cursor(dictionary=True)
+
+    @staticmethod
+    def _parse_dbms_name(dbms_name):
+        try:
+            connector = Database.dbms_connector[dbms_name.lower()]
+            return connector
+        except KeyError:
+            LOG.exception(f"Wrong DBMS {dbms_name}")
+            exit(1)
 
     def __del__(self):
         self._cursor.close()
@@ -26,12 +41,12 @@ class Database:
     def fetch_result(self):
         return self._cursor.fetchall()
 
-    def commit(self):
+    def save(self):
         self._db.commit()
 
     def execute_query(self, query, *args):
         try:
             self._cursor.execute(query, args)
         except mysql.connector.Error as err:
-            LOG.exception("Failed query: {}".format(err))
-            exit(1)
+            self._db.rollback()
+            LOG.exception(f"Failed query: {query}\n{err}")
